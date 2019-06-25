145c7519aeb97b5516c0015cb8f4e0d4c906f9a2
Jonathan <jonathandrewkerr@gmail.com>
2019-06-25 20:48:35 +0100

    Add second test for fizzbuzz
    I've added a more complicated test for fizzbuzz.

--- test_start.py
+++ test_start.py
@@ -4,3 +4,13 @@ import pytest
 @pytest.mark.parametrize("number, output", [(1, 1), (5, "buzz"),(3, "fizz"), (15, "fizzbuzz")])
 def test_fizzbuzzer(number, output):
     assert start.fizz_buzzer(number) == output
+
+
+@pytest.mark.parametrize("number, output", [
+    (3, [1, 2, "fizz"]),
+    (15, [1, 2, "fizz", 4, "buzz", "fizz", 7, 8, 9, "buzz", 11, "fizz", 13, 14, "fizzbuzz"]),
+    ()
+]
+                         )
+def test_fizzbuzz(number, output):
+    assert start.fizzbuzz(number) == output
