d1bbe438deb8fd8e988ae45017719476140d7b86
Jonathan <jonathandrewkerr@gmail.com>
2019-06-25 18:47:51 +0100

    Add missing test
    Need a test to check that fizz_buzzer also returns the number if it's not fizzing, buzzing, or fizzbuzzing

--- test_start.py
+++ test_start.py
@@ -1,6 +1,6 @@
 import start
 import pytest
 
-@pytest.mark.parametrize("number, output", [(5, "buzz"),(3, "fizz"), (15, "fizzbuzz")])
+@pytest.mark.parametrize("number, output", [(1, 1), (5, "buzz"),(3, "fizz"), (15, "fizzbuzz")])
 def test_fizzbuzzer(number, output):
     assert start.fizz_buzzer(number) == output
