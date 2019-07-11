ebb672d9f1b5ff6b7e60a7e609fd62be5aa18c5e
GeorgiaHCA <gaitkenhead@turing.ac.uk>
2019-07-11 18:44:40 +0100

    Implement fizzbuzz
    Created loop to count and outputed a list. Still not fixed for negative numbers.

--- start.py
+++ start.py
@@ -1,5 +1,11 @@
 def fizzbuzz(final_number): #function which outputs fizzbuzz for all ints divisible by 3 and 5
-    pass
+
+    output = []
+
+    for number in range(1, final_number+1):
+         output.append(fizz_buzzer(number))
+    return output
+
 
 def fizz_buzzer(number):
     if number%5==0 and number%3==0:
