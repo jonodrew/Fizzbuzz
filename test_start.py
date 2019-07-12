2efabd77075c6aa10ebcdee6d04b1ba03b86e8c9
GeorgiaHCA <gaitkenhead@turing.ac.uk>
2019-07-12 16:00:17 +0100

    Update test so that it runs with new programme
    Changed test_start.py so that it calls (improved) pythonhelp.py rather than start.py

--- test_start.py
+++ test_start.py
@@ -1,17 +1,19 @@
-import start
+from pythonhelp import FizzBuzz
 import pytest
 
 @pytest.mark.parametrize("number, output", [(1, 1), (5, "buzz"),(3, "fizz"), (15, "fizzbuzz")])
 def test_fizzbuzzer(number, output):
-    assert start.fizz_buzzer(number) == output
+    fizzbuzz = FizzBuzz()
+    assert fizzbuzz.fizz_buzzer(number) == output
 
 
 @pytest.mark.parametrize("number, output", [
     (3, [1, 2, "fizz"]),
     (15, [1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz"]),
     (5, [1, 2, "fizz", 4, "buzz"]),
-    (-5, ["zzub", -4, "zzif", -2, -1, "zzubzzif"]) #fixed spelling mistake - zubb - zzub, added "zzubzif for 0"
+    (-5, ["zzub", -4, "zzif", -2, -1, "zzubzzif"]) #fixed spelling mistake - zubb - zzub, added "zzubzzif for 0"
 ]
                          )
 def test_fizzbuzz(number, output):
-    assert start.fizzbuzz(number) == output
+    fizzbuzz = FizzBuzz()
+    assert fizzbuzz.fizzbuzz(number) == output
